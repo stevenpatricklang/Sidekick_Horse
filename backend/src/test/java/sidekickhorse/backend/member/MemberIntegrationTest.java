3f48bb0a58bd96e39a262247c34162dff7f44b14
stevenpatricklang <stevenpatricklang@gmail.com>
2022-11-21 17:32:30 +0100

    IntegrationTest

---
+++ backend/src/test/java/sidekickhorse/backend/member/MemberIntegrationTest.java
@@ -0,0 +1,65 @@
+package sidekickhorse.backend.member;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+class MemberIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DirtiesContext
+    @Test
+    void addMember() throws Exception {
+
+        // GIVEN
+
+        String body = mvc.perform(MockMvcRequestBuilders.post("/api/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {"firstName": "Steven",
+                                 "lastName": "Lang",
+                                 "street": "Kirchweg 6",
+                                 "zipcode": "86856",
+                                 "city": "Hiltenfingen",
+                                 "email": "steven@gmail.com"}
+                                """))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        Member member = objectMapper.readValue(body, Member.class);
+
+        // WHEN
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/members/"))
+
+        // THEN
+
+        .andExpect(status().isOk())
+        .andExpect(content().json("""
+                         [{"firstName": "Steven",
+                         "lastName": "Lang",
+                         "street": "Kirchweg 6",
+                         "zipcode": "86856",
+                         "city": "Hiltenfingen",
+                         "email": "steven@gmail.com"
+                          "id" : "<id>"}]
+                """.replace("<id>", member.id())));;
+    }
+}
