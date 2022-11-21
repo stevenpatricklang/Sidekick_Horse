380ccf2fb70f830df0b30408c2dc9712e9a3cdfc
stevenpatricklang <stevenpatricklang@gmail.com>
2022-11-21 15:47:21 +0100

    add Method addMember

--- backend/src/main/java/sidekickhorse/backend/member/MemberController.java
+++ backend/src/main/java/sidekickhorse/backend/member/MemberController.java
@@ -1,6 +1,26 @@
 package sidekickhorse.backend.member;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/members")
+
 public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {this.memberService = memberService;}
+
+    @PostMapping
+    Member addMember(@RequestBody @Valid NewMember member) {
+        return memberService.addMemberData(member);
+    }
 
 
 }
