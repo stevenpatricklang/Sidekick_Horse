6ec3cb52f6d4742c729dc2e407395e371c315640
stevenpatricklang <stevenpatricklang@gmail.com>
2022-11-22 12:00:05 +0100

    frontend css + components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import {Route, Routes} from "react-router-dom"
+import Homepage from "./pages/About";
 
-function App() {
-  return (
-    <h1>Mitgliederliste Sidekick Horses</h1>
 
+export default function App() {
+  return <>
+    <StyledHeader>
+            <h1>Welcome to Sidekick Horses Administration</h1>
+      </StyledHeader>
 
-  );
+    <StyledMain>
+      <Routes>
+        <Route path="/" element={<Homepage />}/>
+        <Route path="/members" element={<MembersOverview />}/>
+        <Route path="/about" element={<About />}/>
+      </Routes>
+    </StyledMain>
+
+  <StyledFooter>
+    <p>© 2022 Sidekick Horses</p>
+  </StyledFooter>
+  </>
 }
 
-export default App;
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 .0625rem .5rem 0 rgba(0, 0, 0, .04), 0 .0625rem .3125rem 0 rgba(0, 0, 0, .04);
+  padding: 10px;
+  margin-bottom: 20px;
+  
+  
+const StyledMain = styled.main
+  margin: 50px;
+  min-height: 200px;
+  
+  
+  
+ const StyledFooter = styled.footer
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+  padding: 10px;
+  font-size: 1rem;
+  
