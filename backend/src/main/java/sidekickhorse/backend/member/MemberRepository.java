2ea44aa7a600a8976f3f66efdca46f39cac31697
stevenpatricklang <stevenpatricklang@gmail.com>
2022-11-30 15:00:57 +0100

    add Membership Interface

--- backend/src/main/java/sidekickhorse/backend/member/MemberRepository.java
+++ backend/src/main/java/sidekickhorse/backend/member/MemberRepository.java
@@ -1,6 +1,9 @@
 package sidekickhorse.backend.member;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface MemberRepository extends MongoRepository<Member, String> {
 }
