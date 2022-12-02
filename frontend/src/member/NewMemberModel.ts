fd92093272b7c02b065ca8c5de91e6ff5e43d013
stevenpatricklang <stevenpatricklang@gmail.com>
2022-12-02 15:41:47 +0100

    changed Tests

--- frontend/src/member/NewMemberModel.ts
+++ frontend/src/member/NewMemberModel.ts
@@ -4,5 +4,13 @@ export type MemberModel = {
     street: string;
     zipcode: string;
     city: string;
+    age: string;
     email: string;
+    phoneNumber: string;
+    beginMembership: string;
+    ridingExperience: string;
+    membershipActive: boolean;
+    accountHolder: string;
+    iban: string;
+    bankName: string;
 }
