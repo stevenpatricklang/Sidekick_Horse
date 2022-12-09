8582c3569be75e5e52f48c7b5909d16f0a9d807e
stevenpatricklang <stevenpatricklang@gmail.com>
2022-12-09 12:42:37 +0100

    add AppUser, AppUserController

---
+++ backend/src/main/java/sidekickhorse/backend/security/AppUser.java
@@ -0,0 +1,20 @@
+package sidekickhorse.backend.security;
+
+
+import lombok.With;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@With
+public record AppUser(
+        String id,
+        @NotBlank(message = "User's name cannot be empty.")
+        String username,
+        @NotBlank(message = "User's password cannot be empty.")
+        @Pattern(regexp = "^(?=[^A-Z]*+[A-Z])(?=[^a-z]*+[a-z])(?=\\D*+\\d)(?=[^#?!@$ %^&*-]*+[#?!@$ %^&*-]).{8,}$", message = "Password must have minimum eight characters, at least one letter and one number!")
+        String rawPassword,
+        String passwordBcrypt,
+        AppUserRole role
+) {
+}
