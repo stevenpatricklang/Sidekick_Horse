9988055b3734c783a165c54a7fe5aca61bc96643
stevenpatricklang <stevenpatricklang@gmail.com>
2022-12-12 14:55:51 +0100

    some frontend style

--- frontend/src/member/MemberBoard.tsx
+++ frontend/src/member/MemberBoard.tsx
@@ -1,6 +1,5 @@
-import React, {useState} from 'react';
+import React, {FormEvent, useState} from 'react';
 import {MemberModel} from "./MemberModel";
-import MemberModal from "./MemberModal";
 import styled from "styled-components";
 import axios from "axios";
 
@@ -10,11 +9,55 @@ type MemberCardProps = {
 }
 
 export default function MemberCard(props: MemberCardProps) {
-    const [editModal, setEditModal] = useState(false)
+    const [doEdit, setDoEdit] = useState(false)
     const [messageStatus, setMessageStatus] = useState("")
 
-    function handleEdit() {
-        setEditModal(!editModal)
+    const [newFirstName, setFirstName] = useState(props.member.firstName)
+    const [newLastName, setLastName] = useState(props.member.lastName)
+    const [newStreet, setStreet] = useState(props.member.street)
+    const [newZipcode, setZipcode] = useState(props.member.zipcode)
+    const [newCity, setCity] = useState(props.member.city)
+    const [newAge, setAge] = useState(props.member.age)
+    const [newEmail, setEmail] = useState(props.member.email)
+    const [newPhoneNumber, setPhoneNumber] = useState(props.member.phoneNumber)
+    const [newBeginMembership, setBeginMembership] = useState(props.member.beginMembership)
+    const [newRidingExperience, setRidingExperience] = useState(props.member.ridingExperience)
+    const [newMembershipActive, setMembershipActive] = useState(props.member.membershipActive)
+    const [newAccountHolder, setAccountHolder] = useState(props.member.accountHolder)
+    const [newIBAN, setIBAN] = useState(props.member.iban)
+    const [newBankName, setBankName] = useState(props.member.bankName)
+
+
+    function updateMember(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        axios.put("/api/members/" + props.member.id, {
+            id: props.member.id,
+            firstName: newFirstName,
+            lastName: newLastName,
+            street: newStreet,
+            zipcode: newZipcode,
+            city: newCity,
+            age: newAge,
+            email: newEmail,
+            phoneNumber: newPhoneNumber,
+            beginMembership: newBeginMembership,
+            membershipActive: newMembershipActive,
+            ridingExperience: newRidingExperience,
+            accountHolder: newAccountHolder,
+            iban: newIBAN,
+            bankName: newBankName
+
+        })
+            .then(response => {
+                props.fetchAllMembers()
+                return response.data
+            })
+            .catch(error => console.log(error))
+
+    }
+
+    const checkHandler = () => {
+        setMembershipActive(!newMembershipActive);
     }
 
     const deleteMember = () => {
@@ -30,7 +73,69 @@ export default function MemberCard(props: MemberCardProps) {
             })
             .then(() => setTimeout(() => props.fetchAllMembers(), 2000))
     }
+    if (doEdit) {
+        return (
+            <StyledSection>
+                <StyledForm onSubmit={updateMember}>
+                    <StyledLabel>FirstName</StyledLabel>
+                    <StyledInput type="text" value={newFirstName}
+                                 onChange={event => setFirstName((event.target.value))}/>
 
+                    <StyledLabel>LastName</StyledLabel>
+                    <StyledInput type="text" value={newLastName} onChange={event => setLastName((event.target.value))}/>
+
+                    <StyledLabel>Street</StyledLabel>
+                    <StyledInput type="text" value={newStreet} onChange={event => setStreet((event.target.value))}/>
+
+                    <StyledLabel>Zipcode</StyledLabel>
+                    <StyledInput type="text" value={newZipcode} onChange={event => setZipcode((event.target.value))}/>
+
+                    <StyledLabel>City</StyledLabel>
+                    <StyledInput type="text" value={newCity} onChange={event => setCity((event.target.value))}/>
+
+                    <StyledLabel>Age</StyledLabel>
+                    <StyledInput type="text" value={newAge} onChange={event => setAge((event.target.value))}/>
+
+                    <StyledLabel>Email</StyledLabel>
+                    <StyledInput type="text" value={newEmail} onChange={event => setEmail((event.target.value))}/>
+
+                    <StyledLabel>PhoneNumber</StyledLabel>
+                    <StyledInput type="text" value={newPhoneNumber}
+                                 onChange={event => setPhoneNumber((event.target.value))}/>
+
+                    <StyledLabel>Begin Membership</StyledLabel>
+                    <StyledInput type="text" value={newBeginMembership}
+                                 onChange={event => setBeginMembership((event.target.value))}/>
+
+                    <StyledLabel>Membership Active</StyledLabel>
+                    <input type="checkbox"
+                           checked={newMembershipActive}
+                           onChange={checkHandler}/>
+
+                    <StyledLabel>Riding Experience</StyledLabel>
+                    <StyledInput type="text" value={newRidingExperience}
+                                 onChange={event => setRidingExperience((event.target.value))}/>
+
+                    <StyledLabel>Account Holder</StyledLabel>
+                    <StyledInput type="text" value={newAccountHolder}
+                                 onChange={event => setAccountHolder((event.target.value))}/>
+
+                    <StyledLabel>IBAN</StyledLabel>
+                    <StyledInput type="text" value={newIBAN}
+                                 onChange={event => setIBAN((event.target.value))}/>
+
+                    <StyledLabel>Bank Name</StyledLabel>
+                    <StyledInput type="text" value={newBankName}
+                                 onChange={event => setBankName((event.target.value))}/>
+                    <StyledDiv>
+                        <StyledButton>Submit</StyledButton>
+                        <StyledButton onClick={() => setDoEdit(false)}>Cancel</StyledButton>
+                    </StyledDiv>
+                </StyledForm>
+            </StyledSection>
+        )
+
+    }
     return (
         <>
             <StyledLi>
@@ -53,7 +158,8 @@ export default function MemberCard(props: MemberCardProps) {
                 <StyledRidingExperience> Reiterfahrung: {props.member.ridingExperience}</StyledRidingExperience>
 
                 <StyledMembershipActive> Ist die Mitgliedschaft
-                    aktiv: {props.member.membershipActive}</StyledMembershipActive>
+                    aktiv: <input type="checkbox"
+                                  checked={newMembershipActive}/></StyledMembershipActive>
 
                 <StyledHr/>
                 <StyledAccountHolder>Account Holder: {props.member.accountHolder}</StyledAccountHolder>
@@ -63,19 +169,51 @@ export default function MemberCard(props: MemberCardProps) {
                 <StyledBankName> Name der Bank: {props.member.bankName}</StyledBankName>
 
                 <StyledDiv>
-                    <StyledButton onClick={handleEdit}>Update Member</StyledButton>
+                    <StyledButton onClick={() => setDoEdit(true)}>Update Member</StyledButton>
                     <StyledButton onClick={deleteMember}>Delete</StyledButton>
                 </StyledDiv>
-                {editModal &&
-                    <MemberModal closeModal={handleEdit}
-                                 member={props.member}
-                                 fetchAllMembers={props.fetchAllMembers}/>}
+
                 {messageStatus && <StyledDeleteMessage>{messageStatus}</StyledDeleteMessage>}
             </StyledLi>
         </>
     );
 }
 
+
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  margin: 10px;
+  padding: 8px 20px 25px 20px;
+  border: 1px solid rgba(10 10 10 0.3);
+  border-radius: 1pc;
+  box-shadow: 0 .0625rem .5rem 0 rgba(0, 0, 0, .4), 0 .0625rem .3125rem 0 rgba(0, 0, 0, .4);
+`
+const StyledDiv1 = styled.div`
+  align-content: center;
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  padding: 10px;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  padding: 5px;
+`
+const StyledLabel = styled.label`
+  font-size: 0.8rem;
+`
+const StyledInput = styled.input`
+  margin: 5px;
+  padding: 3px;
+  border-radius: 5px;
+  box-shadow: 0 .0625rem .5rem 0 rgba(0, 0, 0, .04), 0 .0625rem .3125rem 0 rgba(0, 0, 0, .04);
+`;
 
 const StyledHr = styled.hr`
  width: 95%;
