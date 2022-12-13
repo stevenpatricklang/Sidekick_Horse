5d837ee28700cb2cbe649826392451a3c756e623
stevenpatricklang <stevenpatricklang@gmail.com>
2022-12-13 15:58:04 +0100

    registration + login

--- backend/src/main/java/sidekickhorse/backend/security/AppUser.java
+++ backend/src/main/java/sidekickhorse/backend/security/AppUser.java
@@ -12,7 +12,7 @@ public record AppUser(
         String username,
         @NotBlank(message = "Password cannot be empty.")
         @Pattern(regexp = "^(?=[^A-Z]*+[A-Z])(?=[^a-z]*+[a-z])(?=\\D*+\\d)(?=[^#?!@$ %^&*-]*+[#?!@$ %^&*-]).{8,}$", message = "Password must have minimum eight characters, at least one letter and one number!")
-        String rawPassword,
+        String password,
         String passwordBcrypt,
         AppUserRole role
 ) {
