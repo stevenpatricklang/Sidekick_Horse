92c54a77a3104548797a9e262ac839c20d2db76e
stevenpatricklang <stevenpatricklang@gmail.com>
2022-12-14 18:34:08 +0100

    style frontend

--- frontend/src/security/LoginPage.tsx
+++ frontend/src/security/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import axios from "axios";
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 import styled from "styled-components";
 
 type Props = {
@@ -11,7 +11,6 @@ export default function LoginPage(props: Props) {
     const [password, setPassword] = useState<string>("")
     const [messageStatus, setMessageStatus] = useState('')
     const [error, setError] = useState("");
-    const navigate = useNavigate();
 
     const login = () => {
         axios.get("/api/app-users/login", {
